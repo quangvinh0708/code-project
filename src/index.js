a66eb924b0383e18c06defcb8e2500ff7db51bd6
Quang Vinh <doquangvinhbestyasuo@gmail>
2021-11-28 00:17:16 +0700

    a1

--- src/index.js
+++ src/index.js
@@ -8,15 +8,20 @@ import { ThemeProvider } from "@mui/styles";
 import theme from "./common/theme/theme.js";
 import { CssBaseline } from "@mui/material";
 // import "bootstrap/dist/css/bootstrap.min.css";
-
+import { StylesProvider, createGenerateClassName } from "@mui/styles";
+const generateClassName = createGenerateClassName({
+    productionPrefix: "c",
+});
 ReactDOM.render(
     <React.StrictMode>
+        {/* <StylesProvider generateClassName={generateClassName}> */}
         <Provider store={store}>
-            <ThemeProvider theme={theme}>
+            <ThemeProvider theme={theme} generateClassName={generateClassName}>
                 <CssBaseline />
                 <App />
             </ThemeProvider>
         </Provider>
+        {/* </StylesProvider> */}
     </React.StrictMode>,
     document.getElementById("root")
 );
